48f41b93674f704fb48a5dccabe5c4d5c9d5f0dd
elvis <baci.albania@gmail.com>
2024-01-24 11:15:24 +0100

    adjusting image size

--- main.js
+++ main.js
@@ -37,55 +37,44 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Get the "More photos" button element
+    
     let morePhotosButton = document.querySelector(".button1");
 
-    // Add a click event listener to the "More photos" button
+    
     morePhotosButton.addEventListener("click", function () {
-        // Fetch and generate 4 more random photos at the bottom
         fetchPhotos();
     });
 
-    // Get the button element for fetching new photos
     let fetchButton = document.querySelector(".button");
 
-    // Add a click event listener to the fetchButton
     fetchButton.addEventListener("click", function () {
-        // Clear existing photos
         photoContainer.innerHTML = "";
 
-        // Fetch new photos from Picsum Photos API and generate 4 new photos
         fetchPhotos();
     });
 
-    // Function to fetch and generate 4 new photos from Picsum Photos API
     function fetchPhotos() {
-        // Generate 4 random photo URLs
         for (let i = 0; i < 4; i++) {
-            let randomWidth = Math.floor(Math.random() * 800) + 200; // Random width between 200 and 1000
-            let randomHeight = Math.floor(Math.random() * 800) + 200; // Random height between 200 and 1000
+            let randomWidth = Math.floor(Math.random() * 800) + 200; 
+            let randomHeight = Math.floor(Math.random() * 800) + 200; 
             let imgUrl = `https://picsum.photos/${randomWidth}/${randomHeight}`;
-            let description = "Elvis Photos  https://picsum.photos/seed/picsum/200/300"; // You can customize the description
+            let description = "Elvis Photos  https://picsum.photos/seed/picsum/200/300"; 
 
-            // Create image container
             let imageContainer = document.createElement("div");
             imageContainer.className = "image_container";
 
-            // Create image element
+            
             let img = document.createElement("img");
             img.className = "pics";
             img.src = imgUrl;
 
-            // Create description paragraph
             let descriptionParagraph = document.createElement("p");
             descriptionParagraph.className = "static_description";
             descriptionParagraph.textContent = description;
 
-            // Append image and description to the container
             imageContainer.appendChild(img);
             imageContainer.appendChild(descriptionParagraph);
 
-            // Append the image container to the photo container
             photoContainer.appendChild(imageContainer);
         }
     }
